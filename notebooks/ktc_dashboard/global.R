7a29d8231fd4baa50e18dadbd4119f2b07f356f8
jrioross <j.rioross@gmail.com>
2022-01-11 21:32:59 -0600

    Made RDS files, including the dynasty_full.RDS. Also started some exploratory graphs.

---
+++ notebooks/ktc_dashboard/global.R
@@ -0,0 +1,10 @@
+library(tidyverse)
+library(shiny)
+
+ktcPalette <- c("ktcBlue" = "#4DB3E9",
+                "ktcMenuBlue" = "#136C9D",
+                "ktcSelectionBlue" = "#0B5680",
+                "ktcLightGrey" = "#626971",
+                "ktcDarkGrey" = "#4A535C",
+                "ktcLightRed" = "#EE8590",
+                "ktcDarkRed" = "#E85262")
